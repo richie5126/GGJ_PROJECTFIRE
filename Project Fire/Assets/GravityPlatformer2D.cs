7b8b3ee3fa45ded0400990463e896bf1b441e271
Richard Ju <richardju@Richards-MacBook-Pro.local>
2017-01-20 22:35:38 -0800

    Implemented core mechanics

---
+++ Project Fire/Assets/GravityPlatformer2D.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityPlatformer2D : MonoBehaviour {
+
+	// Use this for initialization
+	enum MovementTypes
+	{
+		SINE,
+		TRIANGLE,
+		SQUARE
+	}
+	bool mOrientation;
+	float mGAccel;
+	public CircleCollider2D hitbox;
+	public Rigidbody2D body;
+	public float gravScale;
+	float mVerticalVelocity;
+	bool mDead;
+	public int forwardVelocity;
+	bool mIsTouchingGround;
+
+	MovementTypes currentMode;
+
+	void toggleGravity()
+	{
+			mOrientation = !mOrientation;
+	}
+
+	void Start () {
+		currentMode = MovementTypes.SINE;
+		mOrientation = true;
+		mVerticalVelocity = 0;
+		bool mDead = false;
+		bool mIsTouchingGround = false;
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		gameObject.transform.Translate(Vector2.left * -1.0f * Time.deltaTime);
+		if (mOrientation)
+			body.gravityScale = gravScale;
+		else
+			body.gravityScale = -gravScale;
+		
+		bool isToggled = false;
+		if (Input.GetKeyDown (KeyCode.Space) && !isToggled) {
+			toggleGravity ();
+			isToggled = true;
+		}
+		if (Input.GetKeyUp (KeyCode.Space))
+			isToggled = false;
+
+
+		
+	}
+}
