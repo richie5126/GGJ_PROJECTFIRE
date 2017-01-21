ab367141736f4434fd1d65012de77a9282f72307
Richard Ju <richardju@Richards-MacBook-Pro.local>
2017-01-20 22:46:32 -0800

    Updated again

--- Project Fire/Assets/GravityPlatformer2D.cs
+++ Project Fire/Assets/GravityPlatformer2D.cs
@@ -5,31 +5,40 @@ using UnityEngine;
 public class GravityPlatformer2D : MonoBehaviour {
 
 	// Use this for initialization
-	enum MovementTypes
-	{
-		SINE,
-		TRIANGLE,
-		SQUARE
-	}
+
+	//reference to the assembly containing the player, the ceiling, and the ground
+	public GameObject playerAssembly;
+	//orientation - gravity is upwards or downwards
 	bool mOrientation;
-	float mGAccel;
+
+	//reference to the hitbox of the player
 	public CircleCollider2D hitbox;
+
+	//reference to the player's rigid body
 	public Rigidbody2D body;
+
+	//reference to the strength of gravity
 	public float gravScale;
+
+	//Vy
 	float mVerticalVelocity;
 	bool mDead;
-	public int forwardVelocity;
-	bool mIsTouchingGround;
+	//Vx
+	public float forwardVelocity;
 
-	MovementTypes currentMode;
+	//your current state: sine, triangle, or square.
+	int currentMode;
 
+	//toggle Gravity changes gravity from pos to neg and vice versa
+	//mOrientation is true: you're on the ground. else on the ceiling.
 	void toggleGravity()
 	{
 			mOrientation = !mOrientation;
 	}
-
+	//This code
 	void Start () {
-		currentMode = MovementTypes.SINE;
+		body.gravityScale = 4.0f;
+		currentMode = 2;
 		mOrientation = true;
 		mVerticalVelocity = 0;
 		bool mDead = false;
@@ -39,20 +48,55 @@ public class GravityPlatformer2D : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-
-		gameObject.transform.Translate(Vector2.left * -1.0f * Time.deltaTime);
-		if (mOrientation)
-			body.gravityScale = gravScale;
-		else
-			body.gravityScale = -gravScale;
 		
-		bool isToggled = false;
-		if (Input.GetKeyDown (KeyCode.Space) && !isToggled) {
-			toggleGravity ();
-			isToggled = true;
+
+
+		playerAssembly.transform.Translate (Vector2.left * -forwardVelocity * Time.deltaTime);
+		if (currentMode == 3) {
+			if (mOrientation)
+				body.gravityScale = 1000f;
+			else
+				body.gravityScale = -1000f;
+
+			bool isToggled = false;
+			if (Input.GetKeyDown (KeyCode.Space) && !isToggled) {
+				toggleGravity ();
+				isToggled = true;
+			}
+			if (Input.GetKeyUp (KeyCode.Space))
+				isToggled = false;
 		}
-		if (Input.GetKeyUp (KeyCode.Space))
-			isToggled = false;
+		if (currentMode == 2) {
+			body.gravityScale = 0.0f;
+
+			bool isToggled = false;
+			if (Input.GetKeyDown (KeyCode.Space) && !isToggled) {
+				toggleGravity ();
+				if (mOrientation)
+					body.velocity = new Vector2 (body.velocity.x, forwardVelocity);
+				else
+					body.velocity = new Vector2 (body.velocity.x, -forwardVelocity);
+				
+				isToggled = true;
+			}
+			if (Input.GetKeyUp (KeyCode.Space))
+				isToggled = false;
+		}
+		if(currentMode == 1)
+		{
+			if (mOrientation)
+				body.gravityScale = gravScale;
+			else
+				body.gravityScale = -gravScale;
+		
+			bool isToggled = false;
+			if (Input.GetKeyDown (KeyCode.Space) && !isToggled) {
+				toggleGravity ();
+				isToggled = true;
+			}
+			if (Input.GetKeyUp (KeyCode.Space))
+				isToggled = false;
+		}
 
 
 		
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # GGJ_PROJECTFIRE
+
+To pull from this repository, type git pull while your active directory is this repo.
+
