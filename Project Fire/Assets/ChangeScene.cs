3c2c39e3cb4cadb98cc3ebb6976f2bd6880a70d2
Richard Ju <richardju@Richards-MacBook-Pro.local>
2017-01-22 01:38:12 -0800

    Changed menu to add more thingies

--- Project Fire/Assets/ChangeScene.cs
+++ Project Fire/Assets/ChangeScene.cs
@@ -3,7 +3,33 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour {
+	float timer;
+	bool clicked;
+
+	public AudioSource start;
+	public void Start()
+	{
+		clicked = false;
+		timer = 0.0f;
+	}
+	public void Update()
+	{
+		timer += Time.deltaTime;
+		if (clicked && timer >= 1.0f)
+			SceneManager.LoadScene("Main");
+	}
+
 	public void LoadLevel() {
-		SceneManager.LoadScene("Main");
+		clicked = true;
+		start.Play ();
+		timer = 0.0f;
+	}
+	public void QuitLevel()
+	{
+		Application.Quit();
+	}
+	public void LoadMenu()
+	{
+		SceneManager.LoadScene ("MainMenu");
 	}
 }
