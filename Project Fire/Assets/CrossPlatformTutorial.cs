ad10435c8405a6baada78985984fc70458555b53
Richard Ju <richardju@Richards-MacBook-Pro.local>
2017-01-24 00:43:11 -0800

    Added iOS, Mac OSX, and Windows cross-platform support

---
+++ Project Fire/Assets/CrossPlatformTutorial.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossPlatformTutorial : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		#if UNITY_IOS
+		GetComponentInParent<TutorialRendererMobile>().enabled = true;
+		#endif
+		#if UNITY_STANDALONE
+		GetComponentInParent<TutorialRenderer>().enabled = true;
+		#endif
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
